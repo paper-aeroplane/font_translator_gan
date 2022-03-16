3121d3c5363fa322820f86d1e3a1dc39ea1e5fe3
Daria Aksenova <84663884+paper-aeroplane@users.noreply.github.com>
2022-03-16 15:44:12 +0500

    change --print_freq

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/font --model font_translator_gan --name test_new_dataset --no_dropout --gpu_ids 0 --direction english2russian --batch_size 64
+python train.py --dataroot ./datasets/font --model font_translator_gan --name test_new_dataset --no_dropout --gpu_ids 0 --direction english2russian --batch_size 64 --print_freq 128
