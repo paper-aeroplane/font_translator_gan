da7be7f49d6e200d2f707d8e6c8c7f03fb98b627
Daria Aksenova <84663884+paper-aeroplane@users.noreply.github.com>
2022-04-04 23:07:11 +0500

    change --save_epoch_freq to 2

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ./datasets/font --model font_translator_gan --name test_new_dataset --no_dropout --gpu_ids 0 --direction english2russian --batch_size 64 --print_freq 128 --update_html_freq 128 --display_freq 128
+python train.py --dataroot ./datasets/font --model font_translator_gan --name test_new_dataset --no_dropout --gpu_ids 0 --direction english2russian --batch_size 64 --print_freq 128 --update_html_freq 128 --display_freq 128 --save_epoch_freq 2
