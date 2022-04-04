1d3168f6986fb4eeae7f8a273c5882ec02d84a5c
Alena Gainullina <82896548+tanukiCoder@users.noreply.github.com>
2022-04-04 21:23:15 +0500

    Add new arguments to test.sh
    Add direction and phase arguments, change gpu_ids and name arguments.

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ./datasets/font  --model font_translator_gan  --eval --name MLANH --no_dropout 
+python test.py --dataroot ./datasets/font  --model font_translator_gan  --eval --name test_new_dataset --no_dropout --gpu_ids 0 --phase test_unknown_style --direction english2russian
